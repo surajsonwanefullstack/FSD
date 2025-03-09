3e8bd1db87901023b4d9f2b0f423cc15b976c0d6
Suraj Sonwane <surajsonwanefullstack@gmail.com>
2025-03-09 09:30:06 +0530

    commit

---
+++ March09/quiz/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+const home = document.querySelector(".home")
+console.log(home)
+
+const quizOptions = document.querySelector(".quiz-options")
+console.log(quizOptions.children[0].innerHTML)
+
+})
